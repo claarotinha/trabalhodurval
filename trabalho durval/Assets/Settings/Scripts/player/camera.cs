6b618de5f3cd9a9612e45762684347ace1f77899
claarotinha <clar4costa705@gmail.com>
2025-11-30 00:16:04 -0300

    camera

---
+++ trabalho durval/Assets/Settings/Scripts/player/camera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;       // Player
+    public float smoothSpeed = 5f; // Suavização da câmera
+    public Vector3 offset;         // Distância da câmera para o player
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+    }
+}
